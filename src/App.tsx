3f55dd75350271b840043820df016dde2b063c4c
John Roice Aldeza <jroicealdeza@gmail.com>
2024-05-11 14:54:21 +0800

    Improve UI consistency in App and Navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -53,8 +53,8 @@ function App() {
       return (
         <tr key={`data-${index}`}>
           <td>{data.timeStamp.toLocaleTimeString()}</td>
-          <td>{data.data.distance} </td>
-          <td>{data.data.moisture} </td>
+          <td>{data.data.distance}</td>
+          <td>{data.data.moisture}</td>
           <td>{data.data.vibration}</td>
           <td>
             {determineStatus({
@@ -122,13 +122,13 @@ function App() {
 
             <>
               <div>
-                Vibration: <b>{dataState && dataState.vibration}</b>
+                Vibration: <b>{dataState && dataState.vibration}%</b>
               </div>
               <div>
-                Distance: <b>{dataState && dataState.distance}</b>
+                Distance: <b>{dataState && dataState.distance}%</b>
               </div>
               <div>
-                Moisture: <b>{dataState && dataState.moisture}</b>
+                Moisture: <b>{dataState && dataState.moisture}%</b>
               </div>
             </>
           </div>
